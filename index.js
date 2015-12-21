31872ff7592b03f9b6c0b03f087dd998c18720fc
Tamara Cook <tamara.cook@icloud.com>
2015-12-21 08:51:04 +0100

    Fix loader
    * respect query options
    * removed typos

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 'use strict';
-const codegen = require('swagger-js-codegen');
+const loaderUtils = require('loader-utils');
+const CodeGen = require('swagger-js-codegen').CodeGen;
 
 module.exports = function swaggerLoader(source){
   this.cacheable();
-  console.log(this.inputValues);
-  return source;
+  const query = loaderUtils.parseQuery(this.query);
+  if(!this.inputValue){
+    this.inputValue = JSON.parse(source);
+  }
+  return CodeGen.getAngularCode({
+    swagger: this.inputValue[0],
+    className: query.className || 'Api',
+    moduleName: query.moduleName || 'api'
+  });
 };
