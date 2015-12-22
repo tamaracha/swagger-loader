22e1c7d1a7a3f57f6f73e61dc8e5bd6e9c1360b7
Tamara Cook <tamara.cook@icloud.com>
2015-12-22 07:06:21 +0100

    Validate spec before code generation

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 'use strict';
 const loaderUtils = require('loader-utils');
+const parser = require('swagger-parser');
 const CodeGen = require('swagger-js-codegen').CodeGen;
 
 module.exports = function swaggerLoader(source){
   this.cacheable();
+  let callback = this.async();
   const query = loaderUtils.parseQuery(this.query);
-  if(!this.inputValue){
-    this.inputValue = JSON.parse(source);
-  }
-  return CodeGen.getAngularCode({
-    swagger: this.inputValue[0],
-    className: query.className || 'Api',
-    moduleName: query.moduleName || 'api'
-  });
+  parser.validate(this.inputValue[0])
+  .then(
+    (obj) => {
+      const code = CodeGen.getAngularCode({
+        swagger: obj,
+        className: query.className || 'Api',
+        moduleName: query.moduleName || 'api',
+        esnext: query.esnext ? true : false
+      });
+      return callback(null,code);
+    },
+    (e) => {
+      return callback(e);
+    }
+  );
 };
